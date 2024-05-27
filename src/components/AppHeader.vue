eb1c8fadb756c949ed5974be5dc290a413043845
ClaPascarella <claudiopascarella1996@gmail.com>
2024-05-27 23:09:15 +0200

    file.vue

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<template>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Yu-Gi-Oh Api</h1>
+            </div>
+        </div>
+    </div>
+
+</template>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,17 @@
+<script></script>
+
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 bg-warning" style="height: 100vh;">
+                <div class="container bg-white">
+                    <div class="col-12 d-flex justify-content-center mt-5">
+                        <div class="col-10 bg-dark text-white d-flex justify-content-center mt-3" style="height: 3rem;">
+                            ciao
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
